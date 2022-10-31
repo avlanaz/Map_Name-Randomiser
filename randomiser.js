1d8db9b76297daafa5dc728f598136af2665d82a
avlanaz <87753607+avlanaz@users.noreply.github.com>
2022-10-31 11:52:33 +1100

    Added basic randomiser functions

--- randomiser.js
+++ randomiser.js
@@ -1,21 +1,83 @@
-placeNames = [
-	"london",
-	"new york",
-	"andorra la vella"
-]
+
+
+
+
+
+
+
+
+styleSettings = {
+	glottalApostropheEnabled: true,
+	capitaliseEnabled: true,
+	noConsecutiveConsonants: false,
+
+	consonantFiller: "y",
+}
+
+const VOWELS = "aeiou";
+const CONSONANTS = "qwrtypsdfghjklzxcvbnm";
 
 function randomiseName(name) {
 	var randName = "";
 	var nameLength = name.length;
 	while (nameLength > 0) {
-		var index = Math.floor(Math.random()*nameLength);
+		var index = Math.floor(Math.random() * nameLength);
 		randName += name[index];
-		name = name.substr(0, index) + name.substr(index+1);
+		name = name.substr(0, index) + name.substr(index + 1);
 		nameLength--;
 	}
-	console.log(randName);
+	return randName;
 }
 
-placeNames.forEach(placeName => {
-	randomiseName(placeName);
+function styleName(name) {
+	// Start w/ first letter in
+	name = name.toLowerCase();
+	var newName = name[0].toUpperCase();
+	for (let i = 0; i < name.length - 1; i++) {
+		var letter = name[i];
+		var nextLetter = name[i + 1];
+
+		// Adds accent for two consecutive vowels
+		if (styleSettings.glottalApostropheEnabled) {
+			if (VOWELS.includes(letter) && VOWELS.includes(nextLetter)) {
+				newName += "'";
+			}
+		}
+
+		// Capitalise front letters
+		if (styleSettings.capitaliseEnabled) {
+			if ((letter === " ")) {
+				nextLetter = nextLetter.toUpperCase();
+			}
+		}
+
+		// Add "e" in between consonants
+		if (styleSettings.noConsecutiveConsonants) {
+			if (CONSONANTS.includes(letter) && CONSONANTS.includes(nextLetter)) {
+				newName += styleSettings.consonantFiller;
+			}
+		}
+		newName += nextLetter;
+	}
+	return newName;
+}
+
+capitals.forEach((capital) => {
+	var newCapital = "";
+	newCapital = randomiseName(capital.capital);
+	newCapital = styleName(newCapital);
+	console.log(capital.country + ": " + newCapital);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
